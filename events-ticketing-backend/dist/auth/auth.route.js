f39908e021b5755516560f5368a0bbc4fa2fe8e4
puritychela <puritychelangat18@gmail.com>
2025-07-28 15:51:40 +0300

    changes made

--- events-ticketing-backend/dist/auth/auth.route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const auth_controller_1 = require("./auth.controller");
-const authRouter = (0, express_1.Router)();
-authRouter.post('/auth/register', auth_controller_1.createUser);
-authRouter.post('/auth/login', auth_controller_1.loginUser);
-exports.default = authRouter;
